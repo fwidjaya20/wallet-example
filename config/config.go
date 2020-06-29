d1019fb5fb78f6a953b25ae5c35829bfdb8dd57e
Fredrick Widjaya <f.widjaya20@gmail.com>
2020-06-29 08:53:10 +0700

    Adding Config

---
+++ config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import "os"
+
+// noinspection ALL
+const (
+	ENV 			= "ENV"
+	ENV_DEVELOPMENT = "development"
+	ENV_STAGING 	= "staging"
+	ENV_PRODUCTION 	= "production"
+)
+
+// noinspection ALL
+const (
+	SERVICE_NAME string = "SERVICE_NAME"
+
+	HTTP_ADDR = "HTTP_ADDR"
+
+	DB_DRIVER = "DB_DRIVER"
+	DB_HOST = "DB_HOST"
+	DB_PORT = "DB_PORT"
+	DB_USER = "DB_USER"
+	DB_PASS = "DB_PASS"
+	DB_NAME = "DB_NAME"
+
+	MIGRATION_PATH = "MIGRATION_PATH"
+)
+
+var defaultConfig = map[string]string{
+	// Common Configuration
+	ENV:          ENV_DEVELOPMENT,
+	SERVICE_NAME: "wallet-example",
+
+	// Database Configuration
+	DB_DRIVER: "postgres",
+	DB_HOST:   "localhost",
+	DB_PORT:   "5432",
+	DB_NAME:   "wallet_example",
+	DB_USER:   "postgres",
+	DB_PASS:   "postgres",
+
+	// Migration and Seeder
+	MIGRATION_PATH: "internal/databases",
+
+	// Transport
+	HTTP_ADDR: ":8001",
+}
+
+func GetEnv(key string) string {
+	r := os.Getenv(key)
+
+	if r == "" {
+		if _, ok := defaultConfig[key]; !ok {
+			return ""
+		}
+		r = defaultConfig[key]
+	}
+
+	return r
+}
